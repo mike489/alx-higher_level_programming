25b2c202ae58c85f9c2b53f56d5b2fbaacd53305
mike489 <michaeltesfaye84@gmail.com>
2022-07-19 18:39:34 +0300

    edit

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -19,7 +19,3 @@ def safe_print_integer_err(value):
     except (TypeError, ValueError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (False)
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Term
